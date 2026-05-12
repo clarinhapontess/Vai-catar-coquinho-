a36aa6a164f07badacf71c10c5cfad6a164854db
Dandara Resende <dmsrb@cesar.school>
2026-05-12 17:28:33 -0300

    add sprite player

--- src/player.c
+++ src/player.c
@@ -3,10 +3,16 @@
 
 Player player; // -> cria o player //
 
+// texturas player
+Texture2D crab1Texture;
+
 // Função para inicializar o jogador (onde ele começa)//
 void InitPlayer() {
+    crab1Texture = LoadTexture(
+        "assets/sprites/crabisson1.png"
+    );
     player.x = 450;
-    player.y = 520;
+    player.y = 450;
     player.speed = 7; //-> velocidade do jogador //
     player.width = 100; //-> largura do jogador //
     player.height = 40; //-> altura do jogador //
@@ -22,20 +28,20 @@ void UpdatePlayer() {
         player.x += player.speed;
 
     //delimita o jogador a area visivel da tela//
-    if (player.x < 0)
-    player.x = 0;
+    if (player.x < 45)
+    player.x = 45;
 
-    if (player.x > 900)
-    player.x = 900;
+    if (player.x > 820)
+    player.x = 820;
 }
 
 // Função para desenhar o jogador //
 void DrawPlayer() {
-    DrawRectangle(
-    player.x,
-    player.y,
-    player.width,
-    player.height,
-    RED
-);
+   DrawTextureEx(
+        crab1Texture,
+        (Vector2){player.x, player.y},
+        0.0f,
+        1.0f,
+        WHITE
+    );
 }
